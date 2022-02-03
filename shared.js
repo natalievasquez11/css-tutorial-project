8806ba752ef0525beb23fcc80432102e9382139e
Natalie Vasquez <natalievasquez11@gmail.com>
2022-02-03 15:00:41 -0600

    updates to sidebar - now working on reasons page

--- shared.js
+++ shared.js
@@ -8,33 +8,28 @@ var mobileNav = document.querySelector('.mobile-nav');
  
 for(let i = 0; i < selectPlanButton.length; i++) {
   selectPlanButton[i].addEventListener('click', function() {
-    // modal.style.display = 'block';
-    // backdrop.style.display = 'block';
-    // modal.className = 'open //will override complete class list
     modal.classList.add('open');
     backdrop.classList.add('open');
-    
   });
 }
 
 backdrop.addEventListener('click', function() {
-  // mobileNav.style.display = 'none';
   mobileNav.classList.remove('open');
   closeModal();
 });
 
-closeModalButton.addEventListener('click', closeModal);
+if(closeModalButton) {
+  closeModalButton.addEventListener('click', closeModal);
+}
 
 function closeModal() {
-  // modal.style.display = 'none';
-  // backdrop.style.display = 'none';
-  modal.classList.remove('open');
+  if(modal) {
+    modal.classList.remove('open');
+  }
   backdrop.classList.remove('open');
 }
 
 toggleButton.addEventListener('click', function() {
-  // mobileNav.style.display = 'block';
-  // backdrop.style.display = 'block';
   mobileNav.classList.add('open');
   backdrop.classList.add('open');
 });
