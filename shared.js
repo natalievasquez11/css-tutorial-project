4979bf0e82c8e91b3a6d0ed8e8c781ff6b3e35e6
Natalie Vasquez <natalievasquez11@gmail.com>
2022-02-03 14:48:37 -0600

    sidebar updates

--- shared.js
+++ shared.js
@@ -2,20 +2,40 @@ var backdrop = document.querySelector('.backdrop');
 var selectPlanButton = document.querySelectorAll('.plan button');
 var modal = document.querySelector('.modal');
 var closeModalButton = document.querySelector('.modal__action--negative');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+
  
 for(let i = 0; i < selectPlanButton.length; i++) {
   selectPlanButton[i].addEventListener('click', function() {
-    modal.style.display = 'block';
-    backdrop.style.display = 'block';
+    // modal.style.display = 'block';
+    // backdrop.style.display = 'block';
+    // modal.className = 'open //will override complete class list
+    modal.classList.add('open');
+    backdrop.classList.add('open');
+    
   });
 }
 
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', function() {
+  // mobileNav.style.display = 'none';
+  mobileNav.classList.remove('open');
+  closeModal();
+});
+
 closeModalButton.addEventListener('click', closeModal);
 
 function closeModal() {
-  modal.style.display = 'none';
-  backdrop.style.display = 'none';
+  // modal.style.display = 'none';
+  // backdrop.style.display = 'none';
+  modal.classList.remove('open');
+  backdrop.classList.remove('open');
 }
 
+toggleButton.addEventListener('click', function() {
+  // mobileNav.style.display = 'block';
+  // backdrop.style.display = 'block';
+  mobileNav.classList.add('open');
+  backdrop.classList.add('open');
+});
 
